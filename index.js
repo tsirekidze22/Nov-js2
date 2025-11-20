e1121f37624bf2d8312461b94e620dd03f075a3d
Tsotne Tsirekidze <90200590+tsirekidze22@users.noreply.github.com>
2025-11-20 21:09:43 +0400

    if else & loops

---
+++ index.js
@@ -0,0 +1,111 @@
+// const weekDay = "Sunday";
+
+// if (weekDay === "Thursday") {
+//   console.log("ლექციის დროა");
+// } else {
+//   console.log("დღეს ხუთშაბათი არ არის");
+// }
+
+// const weekDay = "Monday";
+// const age = 66;
+
+// if (age >= 17) {
+//   if (weekDay === "Monday") {
+//     console.log("შეგიძლია გახვიდე მართვის მოწმობაზე");
+//   } else {
+//     console.log("დღეს ორშაბათი არ არის");
+//   }
+// } else {
+//   console.log("ჯერ არ ხარ მინიმუმ 17 წლის.");
+// }
+
+// const grade = 39;
+
+// if (grade >= 90) {
+//   console.log("Grade A, score:", grade);
+// } else if (grade >= 80) {
+//   console.log("Grade B, score:", grade);
+// } else if (grade >= 70) {
+//   console.log("Grade C, score:", grade);
+// } else if (grade >= 60) {
+//   console.log("Grade D, score:", grade);
+// } else if (grade >= 50) {
+//   console.log("Grade E, score:", grade);
+// } else {
+//   console.log("Grade F, try again. score:", grade);
+// }
+
+// const weekDay = "Monday";
+
+// switch (weekDay) {
+//   case "Monday":
+//     console.log("კვირის დასაწყისი");
+//     break;
+//   case "Wednesday":
+//     console.log("კვირის შუა");
+//     break;
+//   case "Saturday":
+//     console.log("შაბათია");
+//     break;
+//   default:
+//     console.log("ერთი ჩვეულებრივი კვირის დღე");
+// }
+
+// const number = 12;
+
+// switch (true) {
+//   case number > 20:
+//     console.log("თვის მიწურული");
+//     break;
+//   case number > 10:
+//     console.log("შუა თვე");
+//     break;
+//   default:
+//     console.log("თვის დასაწყისი");
+// }
+
+// const weekDay = "Monday";
+
+// switch (weekDay) {
+//   case "Monday":
+//   case "Tuesday":
+//   case "Wednesday":
+//   case "Thursday":
+//   case "Friday":
+//     console.log("კვირის სამუშაო დღეა");
+//     break;
+//   default:
+//     console.log("დასვენების დღეა");
+// }
+
+// For, While
+// for (let i = 0; i < 20; i++) {
+//   console.log("<div class='card'>blog title</div>");
+// }
+
+// let number = 0;
+
+// while (number < 5) {
+//   console.log(number);
+//   number++;
+// }
+
+// let password = "123";
+
+// do {
+//   console.log("პაროლი");
+// } while (password !== "secret");
+
+// let number = 10;
+
+// do {
+//   console.log(number);
+// } while (number < 5);
+
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+  sum += i;
+  console.log(sum, i);
+}
+
+// console.log(sum);
